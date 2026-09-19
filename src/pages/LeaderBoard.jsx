9163eec5862bbc631ce162e6235b9ac6813cd48f
Karaj Singh <114018105+karaj368@users.noreply.github.com>
2026-09-19 13:15:30 +0530

    Implement dynamic leaderboard with localStorage

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,10 +1,25 @@
-
+import { useState, useEffect } from "react";
 
 function LeaderBoard() {
+const [data, setData] = useState([]);
+
+useEffect(() =>{
+    const stored = JSON.parse(localStorage.getItem("leaderboard")) || [];
+    setData(stored)
+}, [])
+
+const clearBoard = () =>{
+    localStorage.removeItem("leaderboard");
+    setData([]);
+}
+
   return (
    <div className="container text-center py-5">
         <h2 className="fw-bold mb-4">🏆 Leaderboard</h2>
-        <table className="table table-striped shadow">
+        {data.length === 0 ? (
+            <p className="text-muted">No Scroe yet! Play the Quiz.</p>
+        ) : (
+                <table className="table table-striped shadow">
             <thead className="table-dark">
                 <tr>
                     <th>#</th>
@@ -15,23 +30,25 @@ function LeaderBoard() {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Karaj</td>
-                    <td>1</td>
-                    <td>100%</td>
-                    <td>01/01/2026, 00:00:00</td>
+               {data.map((entry, index) => (
+                 <tr>
+                    <td>{index + 1}</td>
+                    <td>{entry.name}</td>
+                    <td>{entry.score}</td>
+                    <td>{entry.percentage}</td>
+                    <td>{entry.date}</td>
                 </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Singh</td>
-                    <td>1</td>
-                    <td>50%</td>
-                    <td>01/01/2026, 00:00:00</td>
-                </tr>
+               )
+
+            )}
+               
             </tbody>
         </table>
-        <button className="btn btn-danger mt-3">Clear Leaderboard</button>
+        ) }
+        
+        <button
+        onClick={clearBoard}
+        className="btn btn-danger mt-3">Clear Leaderboard</button>
     </div>
   )
 }
